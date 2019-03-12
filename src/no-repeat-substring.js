8623666247106c6d1043ec1fd5ab8ff82bd77601
fengyinchao <fengyinchao@shimo.im>
2019-03-12 11:40:18 +0800

    feat: no-repeat-substring

---
+++ src/no-repeat-substring.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @param {string} string
+ * @returns {number}
+ */
+function getNoRepeatSubString (string) {
+  const map = {}
+  let head = 0
+  return string.split('').reduce((max, tail, index) => {
+    head = map[tail] >= head ? map[tail] + 1 : head
+    map[tail] = index
+    return Math.max(max, index - head + 1)
+  }, 0)
+}
+
+console.log(getNoRepeatSubString('pwwkew'))
