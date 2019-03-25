bdc94abef26501c4408429e50738b9c009a36a6b
fengyinchao <1542963276@qq.com>
2019-03-25 22:39:07 +0800

    update debounce

--- original/throttle-debounce.js
+++ original/throttle-debounce.js
@@ -1,35 +1,55 @@
-function throttle (fn, time, immediate) {
-  let startTime = Date.now()
-  let needRunRightNow = immediate
-  return function () {
-    const args = Array.from(arguments)
-    const currentTime = Date.now()
+function throttle(fn, time, immediate) {
+  let startTime = Date.now();
+  let needRunRightNow = immediate;
+  return function() {
+    const args = Array.from(arguments);
+    const currentTime = Date.now();
     if (needRunRightNow) {
-      fn.apply(null, args)
-      needRunRightNow = false
-      return
+      fn.apply(null, args);
+      needRunRightNow = false;
+      return;
     }
     if (currentTime - startTime > time) {
-      fn.apply(null, args)
-      startTime = currentTime
+      fn.apply(null, args);
+      startTime = currentTime;
     }
-  }
+  };
 }
 
-function debounce (fn, time, immediate) {
-  let startTime = Date.now()
-  let needRunRightNow = immediate
-  return function () {
-    const args = Array.from(arguments)
-    const currentTime = Date.now()
+function debounce(fn, time, immediate) {
+  let startTime = Date.now();
+  let needRunRightNow = immediate;
+  return function() {
+    const args = Array.from(arguments);
+    const currentTime = Date.now();
     if (needRunRightNow) {
-      fn.apply(null, args)
-      needRunRightNow = false
-      return
+      fn.apply(null, args);
+      needRunRightNow = false;
+      return;
     }
     if (currentTime - startTime > time) {
-      fn.apply(null, args)
+      // 缺陷：最後一次触发不会执行
+      fn.apply(null, args);
     }
-    startTime = currentTime
-  }
+    startTime = currentTime;
+  };
+}
+
+function debounce2(fn, time, immediate) {
+  // 不存在最后一次触发不执行的问题
+  let timer = null;
+  let needRunRightNow = immediate;
+  return function() {
+    const args = Array.from(arguments);
+    if (needRunRightNow) {
+      fn.apply(null, args);
+      needRunRightNow = false;
+      return;
+    }
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(null, args);
+      timer = null;
+    }, time);
+  };
 }
